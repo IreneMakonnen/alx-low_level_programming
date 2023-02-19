0ab7d9e112b019d3d06d58f1bbfca472ed798000
Irene Makonnen <irenemakonnen@gmail.com>
2023-02-19 21:30:49 +0300

    A C program that prints a string exactly followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Description: 'Print a string exactly followed by new line'
+ *
+ * Return: ALways 0 (Success)
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }

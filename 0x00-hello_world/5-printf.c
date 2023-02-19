406cde9823a3e9ee73c2a3972e28fb7d84a08ea5
Irene Makonnen <irenemakonnen@gmail.com>
2023-02-19 21:44:17 +0300

    A C program that prints a string exactly

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Print exact string'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

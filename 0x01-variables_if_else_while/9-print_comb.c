ce31d461bc131b4b86f6c45b67fc8c7ef2bdc1c0
Irene Makonnen <irenemakonnen@gmail.com>
2023-02-23 17:57:50 +0300

    Print all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,8 +2,8 @@
 
 /**
  * main - Entry point
- * Description -  Prints all possible combinations of single-digit numbers
- * Return - Always 0 (Success)
+ * Description:  Prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
 */
 
 int main(void)

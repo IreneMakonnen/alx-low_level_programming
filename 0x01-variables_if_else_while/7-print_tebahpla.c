a8be1d0357eb0a56c7180a4a7036540ef232ee3f
Irene Makonnen <irenemakonnen@gmail.com>
2023-02-23 16:49:02 +0300

    Q. 8

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,10 +7,9 @@
 
 int main(void)
 {
-	int ch = 'z';
-
-	for (ch = 'z'; ch >= 'a'; ch--)
-	putchar(ch);
-		putchar('\n');
+	for (char c = 'z'; c >= 'a'; c--)
+	putchar(c);
+	putchar('\n');
+	
 	return (0);
 }

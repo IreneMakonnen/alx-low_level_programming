fb6e413493ff40d05a153ba6869159e34d4efa76
Irene Makonnen <irenemakonnen@gmail.com>
2023-02-23 21:58:20 +0300

    Debugging 0. Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * positive_or_negative - Entry point
+ * Description: Assign random number in variable
+ * Return: Always 0 (Success)
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+	printf("%d is zero\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+}

4aeb2330828373e2458c25aa775951b157a7fe8a
Irene Makonnen <irenemakonnen@gmail.com>
2023-02-27 01:41:35 +0300

    The Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,6 +2,7 @@
 
 /**
  * main - The Fizz-Buzz test
+ * @n; Integer to offer parameters
  * Return: 0
  */
 
@@ -11,17 +12,17 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
 		{
 			printf("Fizz ");
 		}
 		else if (n % 5 == 0)
 		{
 			printf("Buzz ");
-		}
-		else if (n % 3 == 0 && n % 5 == 0)
-		{
-			printf("FizzBuzz ");
 		}
 		else
 		{

4bb0cd4099348e746f45faef7db9cac1c5db4215
Irene Makonnen <irenemakonnen@gmail.com>
2023-03-08 19:56:45 +0300

    Print string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,14 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - 
- * @s:
+ * _print_rev_recursion - Print string in reverse
+ * @s: Reversed string
  */
 
 void _print_rev_recursion(char *s)
 {
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }

d53d414e9bbb6b8aaaa510a18fa268e032c9ab0c
Irene Makonnen <irenemakonnen@gmail.com>
2023-03-08 21:11:10 +0300

    Check if input integer is prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,26 @@
 #include "main.h"
 
 /**
- * is_prime_number - 
- * @n:
+ * is_prime_number - Check if integer is prime number
+ * @n: Number
+ * Return: 1 for success or 0 for fail
  */
 
 int is_prime_number(int n)
 {
+	int i;
+
+	if (n == 0 || n == 1)
+	{
+		return (0);
+	}
+	for (i = 2; i <= n / 2; i++)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+
+	return (1);
 }

79c43f59b3a4fad8b4324a993e553defdf005c16
IreneMakonnen <irenemakonnen@gmail.com>
2023-06-16 00:36:29 +0300

    Program prints size of various data types on computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desription: Prints various types sizes of computer it's compiled & run on
+ * Return: 0
+ */
+
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+
+	printf("Size of char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(longlongintType));
+	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+
+	return (0);
+}

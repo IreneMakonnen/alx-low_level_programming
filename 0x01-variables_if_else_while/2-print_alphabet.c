a4a9479dc1ea88264e20d9a26f148646ab5fca94
IreneMakonnen <irenemakonnen@gmail.com>
2023-06-17 01:57:16 +0300

    Program prints alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 97;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}

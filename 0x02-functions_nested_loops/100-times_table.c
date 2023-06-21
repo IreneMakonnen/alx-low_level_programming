eb5d62cae8fc8bf1302a3683ff45ad9221ceb540
IreneMakonnen <irenemakonnen@gmail.com>
2023-06-22 01:05:22 +0300

    Function prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,38 +7,40 @@ void print_times_table(int n)
 {
 	int a, b, multiply;
 
-	if (n > 15 || n < 0)
+	if (n <= 15 && n >= 0)
 	{
-		return;
-	}
-	for (a = 0; a <= n; a++)
-	{
-		for (b = 0; b <= n; b++)
+		for (a = 0; a <= n; a++)
 		{
-			multiply = a * b;
-			if (b != 0)
-				_putchar(' ');
-			if (multiply <= 9)
+			for (b = 0; b <= n; b++)
 			{
-				_putchar(multiply + '0');
+				multiply = a * b;
+
+				if (b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+
+				if (multiply <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(multiply + '0');
+				}
+				else if (multiply <= 99)
+				{
+					_putchar(' ');
+					_putchar((multiply / 10)+ '0');
+					_putchar((multiply % 10)+ '0');
+				}
+				else
+				{
+					_putchar(multiply / 100 + '0');
+					_putchar((multiply / 10) % 10 + '0');
+					_putchar(multiply % 10 + '0');
+				}
 			}
-			else if (multiply <= 99)
-			{
-				_putchar((multiply / 10) + '0');
-				_putchar((multiply % 10) + '0');
-			}
-			else
-			{
-				_putchar((multiply / 100) + '0');
-				_putchar((multiply / 10) % 10 + '0');
-				_putchar(multiply % 10 + '0');
-			}
-			if (b != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }

bf8b436480e74fa7aab2ef3fa4c0575a8826da26
IreneMakonnen <irenemakonnen@gmail.com>
2023-07-19 02:19:45 +0300

    Defined a new type of structure with 3 elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+
+/**
+ * struct dog - Defining a new type of structure
+ * @name: Element type
+ * @age: Element type
+ * @owner: Element type
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

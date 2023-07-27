ffa592774b52b1ad0bc991fd4074709e36dd12a2
IreneMakonnen <irenemakonnen@gmail.com>
2023-07-28 02:03:56 +0300

    Function prints all elements of a struct list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all elements of a sturcture list
+ * @h: Pointer to string
+ * Return: Number of nodes, [0] (nil) if str = NULL
+ */
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != 0)
+	{
+		if (h->str == 0)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		node_count++;
+	}
+
+	return (node_count);
+}

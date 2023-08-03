54314117e07378160b3dcbdf2c7408dc9d8a96b6
IreneMakonnen <irenemakonnen@gmail.com>
2023-08-03 18:14:56 +0300

    Function checks for endianness (big/ little)

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_endianness - Checks endianness 
+ * get_endianness - Checks endianness
  * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)

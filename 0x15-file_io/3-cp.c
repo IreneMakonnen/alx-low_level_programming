b18c1feca5d57f582cad0eed906052b620d6ff12
IreneMakonnen <irenemakonnen@gmail.com>
2023-08-08 01:45:44 +0300

    Program copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,32 +4,6 @@ int open_file(const char *filename, int flags, mode_t mode);
 void copy_file(int fd_from, int fd_to);
 
 /**
- * main - Entry point for the program
- * @argc: The number of command line arguments
- * @argv: An array of command line argument strings
- * Return: 0 (Success)
- */
-int main(int argc, char *argv[])
-{
-	int fd_from, fd_to;
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fd_from = open_file(argv[1], O_RDONLY, 0);
-	fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	copy_file(fd_from, fd_to);
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
-		exit(100);
-	}
-	return (0);
-}
-
-/**
  * open_file - Opens a file with specified flags and mode
  * @filename: The name of the file
  * @flags: The flags for opening the file
@@ -50,16 +24,16 @@ int open_file(const char *filename, int flags, mode_t mode)
 }
 
 /**
- * copy_file - Copies data from one file descriptor to another.
- * @fd_from: The source file descriptor.
+ * copy_file - Copies data from one file descriptor to another
+ * @fd_from: The source file descriptor
  * @fd_to: The destination file descriptor
  */
 void copy_file(int fd_from, int fd_to)
 {
-	char buffer[BUFFER_SIZE];
+	char buffer[1024];
 	ssize_t read_result, write_result;
 
-	while ((read_result = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	while ((read_result = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		write_result = write(fd_to, buffer, read_result);
 		if (write_result == -1)
@@ -73,4 +47,42 @@ void copy_file(int fd_from, int fd_to)
 		dprintf(STDERR_FILENO, "Error: Can't read from source file\n");
 		exit(98);
 	}
+}
+
+/**
+ * main - Copies the contents of a file to another file
+ * @argc: The number of arguments supplied to the program
+ * @argv: An array of pointers to the arguments
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int from, to;
+	char buffer[1024];
+	size_t read_result, write_result;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	from = open_file(argv[1], O_RDONLY, 0);
+	to = open_file(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((read_result = read(from, buffer, sizeof(buffer))) > 0)
+	{
+		write_result = write(to, buffer, read_result);
+		if (write_result == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (read_result == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+		exit(98);
+	}
+	close(from);
+	close(to);
+	return (0);
 }

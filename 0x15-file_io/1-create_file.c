0096ddc1bc3633382ead35682cf992ad027e83ec
IreneMakonnen <irenemakonnen@gmail.com>
2023-08-08 00:59:42 +0300

    Function creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: Name offile to create
+ * @text_content: NULL terminated string to write to file
+ * Return: 1 (Success)/ -1 (Failure)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int text_len = 0, fd, w;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[text_len])
+			text_len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	w = write(fd, text_content, text_len);
+	if (w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+
+	return (-1);
+}
